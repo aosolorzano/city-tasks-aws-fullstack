c3e3d1ccc437cc52d78d5184c61cfd0a7cef9bdb
Andres Solorzano <andres_solorzano85@hotmail.com>
2023-11-28 20:33:57 -0500

    Adding Amplify configuration for 'dev' environment.

--- apps/city-tasks-app/src/environments/environment.ts
+++ apps/city-tasks-app/src/environments/environment.ts
@@ -7,19 +7,19 @@ export const environment = {
 };
 
 export const defaultDataServiceConfig: DefaultDataServiceConfig = {
-  root: 'https://localhost/api/v1',
+  root: 'city-t-Publi-GqQUR9xpnuXb-1763337452.us-east-1.elb.amazonaws.com/api/v1',
   timeout: 3000,
   delete404OK: false,
   trailingSlashEndpoints: false
 };
 
 export const authConfig: AuthConfig = {
-  issuer: 'https://cognito-idp.IDP_AWS_REGION.amazonaws.com/COGNITO_USER_POOL_ID',
+  issuer: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_cyHEaJxty',
   oidc: true,
   strictDiscoveryDocumentValidation: false,
-  clientId: 'COGNITO_APP_CLIENT_ID_WEB',
-  redirectUri: 'http://localhost/',
+  clientId: '6rmsact7vuhhlks06beu761sea',
+  redirectUri: 'https://dev.d25bs4n1phuxj.amplifyapp.com/', // MUST use the root domain for the Amplify Hosting.
   responseType: 'code',
   scope: 'phone email openid profile aws.cognito.signin.user.admin',
-  showDebugInformation: true
+  showDebugInformation: environment.production
 };
